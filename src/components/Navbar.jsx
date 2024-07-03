0ed949ec48271f0409f14287660fe85dfcdef24d
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-07-03 21:52:49 +0600

    navbar edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,11 @@ const Navbar = () => {
       <div className="navbar bg-base-100 p-0">
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost lg:hidden pl-0"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -45,7 +49,11 @@ const Navbar = () => {
             </ul>
           </div>
           <a className="text-xl">
-            <img src="./logo.svg" alt="logo" />
+            <img
+              src="./logo.svg"
+              alt="logo"
+              className="w-14 h-14 md:w-auto md:h-auto"
+            />
           </a>
         </div>
         <div className="navbar-center hidden lg:flex">
